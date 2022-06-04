0af8aab8d3a511ac10076d77b8b8bcde7725c088
celio natti <celionatti@gmail.com>
2022-06-04 16:29:10 +0100

    Database execute functions method

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -5,12 +5,21 @@ namespace app\controllers;
 use Core\Controller;
 use core\database\Database;
 use core\helpers\CoreHelpers;
+use Exception;
 
 class BlogController extends Controller
 {
+    /**
+     * @throws Exception
+     */
     public function indexAction()
     {
         $db = Database::getInstance();
+        $sql = "INSERT INTO articles (`title`, `body`) VALUES (:title, :body)";
+        $bind = ['title' => 'new article', 'body' => 'article Body'];
+        $query = $db->execute($sql, $bind);
+        $lastId = $query->lastInsertId();
+        CoreHelpers::dnd($lastId);
         $this->view->setSiteTitle('Newest Articles');
         $this->view->render();
     }
--- core/database/Database.php
+++ core/database/Database.php
@@ -8,8 +8,9 @@ use PDO;
 
 class Database
 {
-    protected $_dbh, $_results, $_lastInsertId, $_class;
+    protected $_dbh, $_results, $_class;
     protected int $_fetchType = PDO::FETCH_OBJ;
+    protected mixed $_lastInsertId;
     protected bool $_error = false;
     protected int $_rowCount = 0;
     protected $_stmt;
@@ -42,6 +43,93 @@ class Database
             self::$_db = new self();
         }
         return self::$_db;
+    }
+
+
+    /**
+     * @param $sql
+     * @param array $bind
+     * @return $this
+     */
+    public function execute($sql, array $bind = [])
+    {
+        $this->_results = null;
+        $this->_lastInsertId = null;
+        $this->_error = false;
+        $this->_stmt = $this->_dbh->prepare($sql);
+        if (!$this->_stmt->execute($bind)) {
+            $this->_error = true;
+        } else {
+            $this->_lastInsertId = $this->_dbh->lastInsertId();
+        }
+        return $this;
+    }
+
+
+    /**
+     * @param $sql
+     * @param array $bind
+     * @return $this
+     */
+    public function query($sql, array $bind = [])
+    {
+        $this->execute($sql, $bind);
+        if (!$this->_error) {
+            $this->_rowCount = $this->_stmt->rowCount();
+            if ($this->_fetchType === PDO::FETCH_CLASS) {
+                $this->_results = $this->_stmt->fetchAll($this->_fetchType, $this->_class);
+            } else {
+                $this->_results = $this->_stmt->fetchAll($this->_fetchType);
+            }
+        }
+        return $this;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function results()
+    {
+        return $this->_results;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function count(): int
+    {
+        return $this->_rowCount;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function lastInsertId()
+    {
+        return $this->_lastInsertId;
+    }
+
+    public function setClass($class)
+    {
+        $this->_class = $class;
+    }
+
+    public function getClass()
+    {
+        return $this->_class;
+    }
+
+    public function setFetchType($type)
+    {
+        $this->_fetchType = $type;
+    }
+
+    public function getFetchType()
+    {
+        return $this->_fetchType;
     }
 
 }
