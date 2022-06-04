0f140bd117d6d70398a590cb78a9d67cf1a8c44b
celionatti <amisuusman@gmail.com>
2022-06-04 02:27:54 +0100

    applying commit with autoload

--- index.php
+++ index.php
@@ -2,7 +2,22 @@
 
 session_start();
 
+use \Core\Config;
+
 //define constant
 const PROOT = __DIR__;
 const DS = DIRECTORY_SEPARATOR;
 
+spl_autoload_register(function ($classname){
+    $parts = explode('\\', $classname);
+    $class = end($parts);
+    array_pop($parts);
+    $path = strtolower(implode(DS, $parts));
+    $path = PROOT . DS . $path . DS . $class . '.php';
+    if(file_exists($path)) {
+        include($path);
+    }
+});
+
+$dbname = Config::get('db_name');
+var_dump($dbname);
